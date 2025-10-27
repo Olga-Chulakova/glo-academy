a4f053a041263bbc3f88ad43152e0998ae6abd9a
Olga Chulakova <olka.13@list.ru>
2025-10-28 00:34:11 +0300

    Задание к уроку №2: присвоение значений и работа с методами

---
+++ script.js
@@ -0,0 +1,15 @@
+let title= 'Типы данных';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 30;
+let rollback = 15;
+let fullPrice = 1000;
+let adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
+console.log(screens.toLowerCase().split(', '));
+console.log('Процент отката посреднику за работу ' + (fullPrice * (rollback/100)) + '%');
