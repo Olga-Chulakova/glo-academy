b8728d8b3c22b298501164c71371a58221b3a443
Olga Chulakova <olka.13@list.ru>
2025-10-28 23:15:24 +0300

    добавляет console.log

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 let title = prompt('Как называется ваш проект?');
-
 let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-
 let screenPrice = +prompt('Сколько будет стоить данная работа?', 1200);
-
 let adaptive = confirm('Нужен ли адаптив на сайте?');
-console.log(adaptive);
-
 let service1 = prompt('Какой дополнительный тип услуги нужен?')
 let servicePrice1 = +prompt('Сколько это будет стоить')
-
 let service2 = prompt('Какой дополнительный тип услуги нужен?')
 let servicePrice2 = +prompt('Сколько это будет стоить')
-
 let rollback = 15;
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
 let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
 console.log(servicePercentPrice);
 
@@ -32,3 +24,12 @@ switch(true) {
     default:
         console.log('Что то пошло не так');
 }
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(servicePercentPrice);
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/ долларов/гривен/юани');
+console.log('Стоимость разработки сайта ' + fullPrice + '  рублей/ долларов/гривен/юани');
+console.log('Процент отката посреднику за работу ' + (fullPrice * (rollback/100)));
