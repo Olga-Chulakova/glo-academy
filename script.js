c171ffab317186297f331c7dadd2556c1698b4d1
Olga Chulakova <olka.13@list.ru>
2025-10-28 14:38:33 +0300

    Задание к уроку №3 - типизация, условия, ветвлеения

---
+++ script.js
@@ -0,0 +1,34 @@
+let title = prompt('Как называется ваш проект?');
+
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+
+let screenPrice = +prompt('Сколько будет стоить данная работа?', 1200);
+
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+console.log(adaptive);
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?')
+let servicePrice1 = +prompt('Сколько это будет стоить')
+
+let service2 = prompt('Какой дополнительный тип услуги нужен?')
+let servicePrice2 = +prompt('Сколько это будет стоить')
+
+let rollback = 15;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
+console.log(servicePercentPrice);
+
+switch(true) {
+    case fullPrice > 30000: 
+        console.log('Даем скидку в 10%');
+        break;
+    case fullPrice > 15000 && fullPrice < 30000:
+        console.log('Даем скидку в 5%');
+        break;
+    case fullPrice > 0 && fullPrice < 15000:
+        console.log('Скидка не предусмотрена');
+        break;
+    default:
+        console.log('Что то пошло не так');
+}
